22241d206ed831ae08d3b41f48924bc2f87d53fe
Yohann Prieux <yohannprx@gmail.com>
2025-05-10 16:12:12 +0200

    add appcontext constant & export the function

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,2 +1,8 @@
 import React, { createContext, useContext, useState, useEffect } from "react";
 import { mockCourses, mockArticles, mockSchools } from "../data/mockData";
+
+const AppContext = createContext();
+
+export function useAppContext() {
+  return useContext(AppContext);
+}
