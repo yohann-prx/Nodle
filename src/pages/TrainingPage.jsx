b33b65f2f6a3d650e8ef50eac43c5ff46e7c0e86
Yohann Prieux <yohannprx@gmail.com>
2025-05-10 16:04:35 +0200

    import libraries

---
+++ src/pages/TrainingPage.jsx
@@ -0,0 +1,5 @@
+import React, { useState } from "react";
+import { motion } from "framer-motion";
+import { Search, MapPin, Filter, Grid, List } from "lucide-react";
+import SchoolCard from "../components/common/SchoolCard";
+import { useAppContext } from "../context/AppContext";
