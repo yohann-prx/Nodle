d1398d86ff3b10c3033f2ec8c4a310feb3d4e61f
Loann Duval <l_duval6@hetic.eu>
2025-05-10 16:28:27 +0200

    import Layout component

---
+++ src/components/layout/Layout.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Navbar from './Navbar';
+import Footer from './Footer';
+import { useLocation } from 'react-router-dom';
