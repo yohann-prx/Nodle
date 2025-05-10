41742c8b275f2f8b36d47b22f8958d7b207009b0
Loann Duval <l_duval6@hetic.eu>
2025-05-10 16:33:43 +0200

    Update ProfileSidebar component

--- src/components/profile/ProfileSidebar.jsx
+++ src/components/profile/ProfileSidebar.jsx
@@ -37,7 +37,45 @@ const ProfileSidebar = () => {
           </nav>
         </div>
         
+        <div className="bg-white p-4 rounded-lg mb-4">
+          <h2 className="font-medium text-gray-500 text-sm mb-3">Une question ?</h2>
+          <nav>
+            <a href="#" className="sidebar-link">
+              <MessageSquare className="h-4 w-4" />
+              <span>Nous contacter</span>
+            </a>
+            <a href="#" className="sidebar-link">
+              <ThumbsUp className="h-4 w-4" />
+              <span>Feedback</span>
+            </a>
+          </nav>
+        </div>
         
+        <div className="bg-white p-4 rounded-lg">
+          <h2 className="font-medium text-gray-500 text-sm mb-3">Autres</h2>
+          <nav>
+            <a href="#" className="sidebar-link">
+              <Users className="h-4 w-4" />
+              <span>Utilisateurs bloqués</span>
+            </a>
+            <a href="#" className="sidebar-link">
+              <Eye className="h-4 w-4 text-gray-600" />
+              <span>Catégories masquées</span>
+            </a>
+            <a href="#" className="sidebar-link">
+              <RotateCcw className="h-4 w-4" />
+              <span>Réinitialiser la veille</span>
+            </a>
+            <a href="#" className="sidebar-link text-error-dark">
+              <Trash className="h-4 w-4" />
+              <span>Supprimer votre compte</span>
+            </a>
+            <a href="#" className="sidebar-link text-gray-600">
+              <LogOut className="h-4 w-4" />
+              <span>Se déconnecter</span>
+            </a>
+          </nav>
+        </div>
       </aside>
     );
   };
