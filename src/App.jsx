bd62c411e996c261dcc59e429e07671692bf1122
Yohann Prieux <yohannprx@gmail.com>
2025-05-10 16:26:15 +0200

    add all routes

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,16 @@ import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
   return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="decouverte" element={<DiscoveryPage />} />
+        <Route path="formations" element={<TrainingPage />} />
+        <Route path="profil" element={<ProfilePage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
