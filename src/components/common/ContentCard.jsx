96300b1f405429903f918e4bc60ef023cf41a0e0
Ryan Sellier <r.yansellier@hotmail.com>
2025-05-10 15:32:25 +0200

    Import ContentCard component

---
+++ src/components/common/ContentCard.jsx
@@ -0,0 +1,4 @@
+import React from 'react';
+import { Play, Bookmark, BookmarkCheck } from 'lucide-react';
+import { useAppContext } from '../../context/AppContext';
+import { motion } from 'framer-motion';
