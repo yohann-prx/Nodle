e5c45b7e2adaff8ca011df757efb42e98f257225
Loann Duval <l_duval6@hetic.eu>
2025-05-10 16:30:06 +0200

    import Navbar component

---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,5 @@
+import React, { useState } from 'react';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
+import { Search, User, Menu, X } from 'lucide-react';
+import { useAppContext } from '../../context/AppContext';
+import NodleLogo from '../common/NodleLogo';
