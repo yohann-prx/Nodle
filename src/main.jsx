fa2807ba7d5a8ecb2cd2c1d22c21cb276fa671d6
Yohann Prieux <yohannprx@gmail.com>
2025-05-10 16:30:41 +0200

    add createroot

--- src/main.jsx
+++ src/main.jsx
@@ -4,3 +4,13 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import { AppProvider } from "./context/AppContext";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <BrowserRouter>
+      <AppProvider>
+        <App />
+      </AppProvider>
+    </BrowserRouter>
+  </StrictMode>,
+);
