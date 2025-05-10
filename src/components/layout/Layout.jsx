1ac1044ba749e135b08eb1b047ae9c4466a3e1b8
Loann Duval <l_duval6@hetic.eu>
2025-05-10 16:28:57 +0200

    Create Layout component

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -3,3 +3,20 @@ import { Outlet } from 'react-router-dom';
 import Navbar from './Navbar';
 import Footer from './Footer';
 import { useLocation } from 'react-router-dom';
+
+const Layout = () => {
+    const location = useLocation();
+    const isProfilePage = location.pathname === '/profil';
+    
+    return (
+      <div className="flex flex-col min-h-screen">
+        <Navbar />
+        <main className={`flex-grow ${isProfilePage ? 'bg-gray-50' : ''}`}>
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
+    );
+  };
+  
+  export default Layout;
