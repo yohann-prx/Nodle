fc262d27a72c815777e39ea893dd1f9edba94aac
Ryan Sellier <r.yansellier@hotmail.com>
2025-05-10 15:59:55 +0200

    const topics Sidebar component

--- src/components/home/Sidebar.jsx
+++ src/components/home/Sidebar.jsx
@@ -1,2 +1,39 @@
 import React, { useState } from 'react';
 import { ChevronDown, ChevronUp } from 'lucide-react';
+
+const topics = [
+    {
+      id: 'creativity',
+      name: 'CRÉATIVITÉ & DESIGN',
+      subtopics: [
+        { id: 'photo', name: 'Photographie & Retouche' },
+        { id: '3d', name: '3D' },
+        { id: 'graphic', name: 'Graphisme & Print' },
+        { id: 'video', name: 'Réalisation & Montage' },
+        { id: 'audio', name: 'Audio & MAO' },
+        { id: 'design', name: 'Design' },
+        { id: 'uxui', name: 'UX/UI design' },
+        { id: 'animation', name: 'Animation & Motion design' },
+        { id: 'digital-painting', name: 'Digital painting' },
+        { id: 'cao', name: 'CAO' }
+      ]
+    },
+    {
+      id: 'dev',
+      name: 'DÉVELOPPEMENT & IT',
+      subtopics: [
+        { id: 'web', name: 'Web' },
+        { id: 'programming', name: 'Programmation' },
+        { id: 'systems', name: 'Système d\'exploitation' }
+      ]
+    },
+    {
+      id: 'business',
+      name: 'BUSINESS',
+      subtopics: [
+        { id: 'marketing', name: 'Marketing' },
+        { id: 'communication', name: 'Communication' },
+        { id: 'management', name: 'Management' }
+      ]
+    }
+  ];
