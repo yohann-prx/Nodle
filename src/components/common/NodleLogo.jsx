ba68fb92be24ad60b7ec777a38b30cbcf74b2a35
Ryan Sellier <r.yansellier@hotmail.com>
2025-05-10 15:48:54 +0200

    const NodleLogo component

--- src/components/common/NodleLogo.jsx
+++ src/components/common/NodleLogo.jsx
@@ -1,2 +1,9 @@
 import React from 'react';
 import { Box } from 'lucide-react';
+
+const NodleLogo = ({ size = 'md' }) => {
+    const sizeClasses = {
+      sm: 'h-6',
+      md: 'h-8',
+      lg: 'h-10'
+    };
