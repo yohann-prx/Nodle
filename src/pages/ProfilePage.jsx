c0906bf6506a5dc28c42824a3b54be621cabb0f1
Yohann Prieux <yohannprx@gmail.com>
2025-05-10 15:58:52 +0200

    import libraries

---
+++ src/pages/ProfilePage.jsx
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+import { ChevronDown, Plus } from "lucide-react";
+import { useAppContext } from "../context/AppContext";
+import ProfileSidebar from "../components/profile/ProfileSidebar";
+import ContentCard from "../components/common/ContentCard";
+import ProgressChart from "../components/profile/ProgressChart";
+import { motion } from "framer-motion";
